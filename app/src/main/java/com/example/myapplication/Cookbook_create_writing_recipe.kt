ea0486350e9d89115e7cacf131acdf67efa72985
brain0726 <brain726@naver.com>
2022-11-07 10:59:37 +0900

    merge with senior

---
+++ app/src/main/java/com/example/myapplication/Cookbook_create_writing_recipe.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cookbook_create_writing_recipe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cookbook_create_writing_recipe)
+    }
+}
